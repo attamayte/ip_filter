170c875a5cfb43cdfa83aa2dcaae57677cd72899
Alex Svetlyy <36406930+attamayte@users.noreply.github.com>
2019-10-24 20:04:50 +0300

    Add files via upload

--- lib.h
+++ lib.h
@@ -28,12 +28,12 @@ ip get_ip(std::string& str);
 void populate(std::istream& is, ip_pool& pool);
 
 // format and output
-inline void out(std::ostream& os, const ip& i);
+void out(std::ostream& os, const ip& i);
 
-inline void print(std::ostream& os, ip_pool_ci begin, ip_pool_ci end);
+void print(std::ostream& os, ip_pool_ci begin, ip_pool_ci end);
 
 // a better-looking print call
-inline void print(std::ostream& os, const ip_pool& pool);
+void print(std::ostream& os, const ip_pool& pool);
 
 // filter 1 (& clojure for filter 2)
 void variadic_filter(std::ostream& os, ip_pool_ci begin, ip_pool_ci end, const int pos, const char byte);
